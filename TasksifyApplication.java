ac9c1fda3a32d0a8cb5336abf0b03cd2ad9ad921
Emmanuel Oreofe Oyewole <nathanemmanueloreofe@outlook.com>
2024-06-05 19:07:43 +0100

    Added JAVA files via upload..

---
+++ TasksifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.tasksify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasksifyApplication.class, args);
+	}
+
+}
